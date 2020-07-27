531323cd56d80c319413a2efd4727dbbfcd49266
WalterStern1 <fasterpermits@Fasters-MacBook-Pro-6.local>
2020-07-26 20:12:19 -0700

    app.py commit

--- env/app.py
+++ env/app.py
@@ -1,6 +1,8 @@
-# import necessary libraries
 from models import create_classes
 import os
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 from flask import (
     Flask,
     render_template,
@@ -8,73 +10,95 @@ from flask import (
     request,
     redirect)
 
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
 from flask_sqlalchemy import SQLAlchemy
+engine = create_engine("sqlite:///nsBorder.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///db.sqlite"
 
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+print(Base.classes.keys())
 
-Pet = create_classes(db)
+northPorts = Base.classes.northPorts
+southPorts = Base.classes.southPorts
+northStates = Base.classes.northStates
+southStates = Base.classes.southStates
 
-# create route that renders index.html template
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
 
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
+@app.route("/north_port")
+def north_port_entrydata():
+    session = Session(engine)
+    north_port_results = session.query(northPorts.A, northPorts.PortName, northPorts.Date, northPorts.Value).all()
+    north_port_info = []
+    for result in north_port_results:
+        north_port_entry_info = {
+            "id": result.A,
+            "port_name": result.PortName,
+            "date": result.Date,
+            "value": result.Value
 
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
-
-    return render_template("form.html")
-
-
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
-
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
         }
-    }]
 
-    return jsonify(pet_data)
+        north_port_info.append(north_port_entry_info)
 
+    return jsonify(north_port_info)
 
+@app.route("/south_port")   
+def south_port_entrydata():
+    session = Session(engine)
+    south_port_results = session.query(southPorts.A, southPorts.PortName, southPorts.Date, southPorts.Value).all()
+    south_port_info = []
+    for result in south_port_results:
+        south_port_entry_info = {
+            "id": result.A,
+            "port_name": result.PortName,
+            "date": result.Date,
+            "value": result.Value,
+
+        }
+
+        south_port_info.append(south_port_entry_info)
+
+    return jsonify(south_port_info)
+
+@app.route("/state")
+def state_entrydata():
+    session = Session(engine)
+    north_state_results = session.query(northStates.A, northStates.State, northStates.Date, northStates.Value).all()
+    north_state_info = []
+    for result in north_state_results:
+        north_state_entry_info = {
+            "id": result.A,
+            "state": result.State,
+            "date": result.Date,
+            "value": result.Value,
+
+        }
+        
+        north_state_info.append(north_state_entry_info)
+      
+    
+    south_state_results = session.query(southStates.A, southStates.State, southStates.Date, southStates.Value).all()
+    south_state_info = []
+    for result in south_state_results:
+        south_state_entry_info = {
+            "id": result.A,
+            "state": result.State,
+            "date": result.Date,
+            "value": result.Value
+
+        }
+
+        south_state_info.append(south_state_entry_info)
+    return jsonify(north_state_info, south_state_info)
+    
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
